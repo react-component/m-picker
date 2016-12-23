ee6ec815b48d2d454989b0f2134f2a89aa6de73d
yiminghe <yiminghe@gmail.com>
2016-12-24 03:08:14 +0800

    Updates

--- examples/popup.js
+++ examples/popup.js
@@ -8,6 +8,429 @@ webpackJsonp([2],{
 
 /***/ },
 
+/***/ 3:
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+	
+	var _extends2 = __webpack_require__(4);
+	
+	var _extends3 = _interopRequireDefault(_extends2);
+	
+	var _react = __webpack_require__(42);
+	
+	var _react2 = _interopRequireDefault(_react);
+	
+	var _classnames = __webpack_require__(73);
+	
+	var _classnames2 = _interopRequireDefault(_classnames);
+	
+	var _Picker = __webpack_require__(74);
+	
+	var _Picker2 = _interopRequireDefault(_Picker);
+	
+	var _MultiPickerMixin = __webpack_require__(86);
+	
+	var _MultiPickerMixin2 = _interopRequireDefault(_MultiPickerMixin);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	var MultiPicker = _react2.default.createClass({
+	    displayName: 'MultiPicker',
+	
+	    mixins: [_MultiPickerMixin2.default],
+	    render: function render() {
+	        var _this = this;
+	
+	        var props = this.props;
+	        var prefixCls = props.prefixCls,
+	            pickerPrefixCls = props.pickerPrefixCls,
+	            className = props.className,
+	            rootNativeProps = props.rootNativeProps,
+	            disabled = props.disabled,
+	            pickerItemStyle = props.pickerItemStyle,
+	            indicatorStyle = props.indicatorStyle,
+	            pure = props.pure,
+	            children = props.children;
+	
+	        var selectedValue = this.getValue();
+	        var colElements = children.map(function (col, i) {
+	            return _react2.default.createElement(
+	                'div',
+	                { key: col.key || i, className: prefixCls + '-item' },
+	                _react2.default.createElement(_Picker2.default, (0, _extends3.default)({ itemStyle: pickerItemStyle, disabled: disabled, pure: pure, indicatorStyle: indicatorStyle, prefixCls: pickerPrefixCls, selectedValue: selectedValue[i], onValueChange: _this.onValueChange.bind(_this, i) }, col.props))
+	            );
+	        });
+	        return _react2.default.createElement(
+	            'div',
+	            (0, _extends3.default)({}, rootNativeProps, { className: (0, _classnames2.default)(className, prefixCls) }),
+	            colElements
+	        );
+	    }
+	});
+	exports.default = MultiPicker;
+	module.exports = exports['default'];
+
+/***/ },
+
+/***/ 4:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	
+	exports.__esModule = true;
+	
+	var _assign = __webpack_require__(5);
+	
+	var _assign2 = _interopRequireDefault(_assign);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	exports.default = _assign2.default || function (target) {
+	  for (var i = 1; i < arguments.length; i++) {
+	    var source = arguments[i];
+	
+	    for (var key in source) {
+	      if (Object.prototype.hasOwnProperty.call(source, key)) {
+	        target[key] = source[key];
+	      }
+	    }
+	  }
+	
+	  return target;
+	};
+
+/***/ },
+
+/***/ 5:
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = { "default": __webpack_require__(6), __esModule: true };
+
+/***/ },
+
+/***/ 6:
+/***/ function(module, exports, __webpack_require__) {
+
+	__webpack_require__(7);
+	module.exports = __webpack_require__(10).Object.assign;
+
+/***/ },
+
+/***/ 7:
+/***/ function(module, exports, __webpack_require__) {
+
+	// 19.1.3.1 Object.assign(target, source)
+	var $export = __webpack_require__(8);
+	
+	$export($export.S + $export.F, 'Object', {assign: __webpack_require__(23)});
+
+/***/ },
+
+/***/ 23:
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	// 19.1.2.1 Object.assign(target, source, ...)
+	var getKeys  = __webpack_require__(24)
+	  , gOPS     = __webpack_require__(39)
+	  , pIE      = __webpack_require__(40)
+	  , toObject = __webpack_require__(41)
+	  , IObject  = __webpack_require__(28)
+	  , $assign  = Object.assign;
+	
+	// should work with symbols and should have deterministic property order (V8 bug)
+	module.exports = !$assign || __webpack_require__(19)(function(){
+	  var A = {}
+	    , B = {}
+	    , S = Symbol()
+	    , K = 'abcdefghijklmnopqrst';
+	  A[S] = 7;
+	  K.split('').forEach(function(k){ B[k] = k; });
+	  return $assign({}, A)[S] != 7 || Object.keys($assign({}, B)).join('') != K;
+	}) ? function assign(target, source){ // eslint-disable-line no-unused-vars
+	  var T     = toObject(target)
+	    , aLen  = arguments.length
+	    , index = 1
+	    , getSymbols = gOPS.f
+	    , isEnum     = pIE.f;
+	  while(aLen > index){
+	    var S      = IObject(arguments[index++])
+	      , keys   = getSymbols ? getKeys(S).concat(getSymbols(S)) : getKeys(S)
+	      , length = keys.length
+	      , j      = 0
+	      , key;
+	    while(length > j)if(isEnum.call(S, key = keys[j++]))T[key] = S[key];
+	  } return T;
+	} : $assign;
+
+/***/ },
+
+/***/ 24:
+/***/ function(module, exports, __webpack_require__) {
+
+	// 19.1.2.14 / 15.2.3.14 Object.keys(O)
+	var $keys       = __webpack_require__(25)
+	  , enumBugKeys = __webpack_require__(38);
+	
+	module.exports = Object.keys || function keys(O){
+	  return $keys(O, enumBugKeys);
+	};
+
+/***/ },
+
+/***/ 25:
+/***/ function(module, exports, __webpack_require__) {
+
+	var has          = __webpack_require__(26)
+	  , toIObject    = __webpack_require__(27)
+	  , arrayIndexOf = __webpack_require__(31)(false)
+	  , IE_PROTO     = __webpack_require__(35)('IE_PROTO');
+	
+	module.exports = function(object, names){
+	  var O      = toIObject(object)
+	    , i      = 0
+	    , result = []
+	    , key;
+	  for(key in O)if(key != IE_PROTO)has(O, key) && result.push(key);
+	  // Don't enum bug & hidden keys
+	  while(names.length > i)if(has(O, key = names[i++])){
+	    ~arrayIndexOf(result, key) || result.push(key);
+	  }
+	  return result;
+	};
+
+/***/ },
+
+/***/ 26:
+/***/ function(module, exports) {
+
+	var hasOwnProperty = {}.hasOwnProperty;
+	module.exports = function(it, key){
+	  return hasOwnProperty.call(it, key);
+	};
+
+/***/ },
+
+/***/ 27:
+/***/ function(module, exports, __webpack_require__) {
+
+	// to indexed object, toObject with fallback for non-array-like ES3 strings
+	var IObject = __webpack_require__(28)
+	  , defined = __webpack_require__(30);
+	module.exports = function(it){
+	  return IObject(defined(it));
+	};
+
+/***/ },
+
+/***/ 28:
+/***/ function(module, exports, __webpack_require__) {
+
+	// fallback for non-array-like ES3 and non-enumerable old V8 strings
+	var cof = __webpack_require__(29);
+	module.exports = Object('z').propertyIsEnumerable(0) ? Object : function(it){
+	  return cof(it) == 'String' ? it.split('') : Object(it);
+	};
+
+/***/ },
+
+/***/ 29:
+/***/ function(module, exports) {
+
+	var toString = {}.toString;
+	
+	module.exports = function(it){
+	  return toString.call(it).slice(8, -1);
+	};
+
+/***/ },
+
+/***/ 30:
+/***/ function(module, exports) {
+
+	// 7.2.1 RequireObjectCoercible(argument)
+	module.exports = function(it){
+	  if(it == undefined)throw TypeError("Can't call method on  " + it);
+	  return it;
+	};
+
+/***/ },
+
+/***/ 31:
+/***/ function(module, exports, __webpack_require__) {
+
+	// false -> Array#indexOf
+	// true  -> Array#includes
+	var toIObject = __webpack_require__(27)
+	  , toLength  = __webpack_require__(32)
+	  , toIndex   = __webpack_require__(34);
+	module.exports = function(IS_INCLUDES){
+	  return function($this, el, fromIndex){
+	    var O      = toIObject($this)
+	      , length = toLength(O.length)
+	      , index  = toIndex(fromIndex, length)
+	      , value;
+	    // Array#includes uses SameValueZero equality algorithm
+	    if(IS_INCLUDES && el != el)while(length > index){
+	      value = O[index++];
+	      if(value != value)return true;
+	    // Array#toIndex ignores holes, Array#includes - not
+	    } else for(;length > index; index++)if(IS_INCLUDES || index in O){
+	      if(O[index] === el)return IS_INCLUDES || index || 0;
+	    } return !IS_INCLUDES && -1;
+	  };
+	};
+
+/***/ },
+
+/***/ 32:
+/***/ function(module, exports, __webpack_require__) {
+
+	// 7.1.15 ToLength
+	var toInteger = __webpack_require__(33)
+	  , min       = Math.min;
+	module.exports = function(it){
+	  return it > 0 ? min(toInteger(it), 0x1fffffffffffff) : 0; // pow(2, 53) - 1 == 9007199254740991
+	};
+
+/***/ },
+
+/***/ 33:
+/***/ function(module, exports) {
+
+	// 7.1.4 ToInteger
+	var ceil  = Math.ceil
+	  , floor = Math.floor;
+	module.exports = function(it){
+	  return isNaN(it = +it) ? 0 : (it > 0 ? floor : ceil)(it);
+	};
+
+/***/ },
+
+/***/ 34:
+/***/ function(module, exports, __webpack_require__) {
+
+	var toInteger = __webpack_require__(33)
+	  , max       = Math.max
+	  , min       = Math.min;
+	module.exports = function(index, length){
+	  index = toInteger(index);
+	  return index < 0 ? max(index + length, 0) : min(index, length);
+	};
+
+/***/ },
+
+/***/ 35:
+/***/ function(module, exports, __webpack_require__) {
+
+	var shared = __webpack_require__(36)('keys')
+	  , uid    = __webpack_require__(37);
+	module.exports = function(key){
+	  return shared[key] || (shared[key] = uid(key));
+	};
+
+/***/ },
+
+/***/ 36:
+/***/ function(module, exports, __webpack_require__) {
+
+	var global = __webpack_require__(9)
+	  , SHARED = '__core-js_shared__'
+	  , store  = global[SHARED] || (global[SHARED] = {});
+	module.exports = function(key){
+	  return store[key] || (store[key] = {});
+	};
+
+/***/ },
+
+/***/ 37:
+/***/ function(module, exports) {
+
+	var id = 0
+	  , px = Math.random();
+	module.exports = function(key){
+	  return 'Symbol('.concat(key === undefined ? '' : key, ')_', (++id + px).toString(36));
+	};
+
+/***/ },
+
+/***/ 38:
+/***/ function(module, exports) {
+
+	// IE 8- don't enum bug keys
+	module.exports = (
+	  'constructor,hasOwnProperty,isPrototypeOf,propertyIsEnumerable,toLocaleString,toString,valueOf'
+	).split(',');
+
+/***/ },
+
+/***/ 39:
+/***/ function(module, exports) {
+
+	exports.f = Object.getOwnPropertySymbols;
+
+/***/ },
+
+/***/ 40:
+/***/ function(module, exports) {
+
+	exports.f = {}.propertyIsEnumerable;
+
+/***/ },
+
+/***/ 41:
+/***/ function(module, exports, __webpack_require__) {
+
+	// 7.1.13 ToObject(argument)
+	var defined = __webpack_require__(30);
+	module.exports = function(it){
+	  return Object(defined(it));
+	};
+
+/***/ },
+
+/***/ 86:
+/***/ function(module, exports) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+	exports.default = {
+	    getDefaultProps: function getDefaultProps() {
+	        return {
+	            prefixCls: 'rmc-multi-picker',
+	            pickerPrefixCls: 'rmc-picker',
+	            onValueChange: function onValueChange() {},
+	
+	            disabled: false
+	        };
+	    },
+	    getValue: function getValue() {
+	        if (this.props.selectedValue) {
+	            return this.props.selectedValue;
+	        } else {
+	            return this.props.children.map(function (c) {
+	                return c.props.children[0].value;
+	            });
+	        }
+	    },
+	    onValueChange: function onValueChange(i, v) {
+	        var value = this.getValue().concat();
+	        value[i] = v;
+	        this.props.onValueChange(value, i);
+	    }
+	};
+	module.exports = exports['default'];
+
+/***/ },
+
 /***/ 234:
 /***/ function(module, exports, __webpack_require__) {
 
@@ -25,17 +448,24 @@ webpackJsonp([2],{
 	
 	var _reactDom2 = _interopRequireDefault(_reactDom);
 	
+	var _MultiPicker = __webpack_require__(3);
+	
+	var _MultiPicker2 = _interopRequireDefault(_MultiPicker);
+	
 	var _Popup = __webpack_require__(236);
 	
 	var _Popup2 = _interopRequireDefault(_Popup);
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
+	/* tslint:disable:no-console */
+	var colData = [{ label: '1', value: '1' }, { label: '2', value: '2' }];
 	var Demo = _react2.default.createClass({
 	    displayName: 'Demo',
 	    getInitialState: function getInitialState() {
 	        return {
-	            disabled: false
+	            disabled: false,
+	            value: null
 	        };
 	    },
 	    disable: function disable() {
@@ -43,8 +473,11 @@ webpackJsonp([2],{
 	            disabled: !this.state.disabled
 	        });
 	    },
-	    onOk: function onOk() {
-	        console.log('onOk');
+	    onOk: function onOk(value) {
+	        console.log('onOk', value);
+	        this.setState({
+	            value: value
+	        });
 	    },
 	    onDismiss: function onDismiss() {
 	        console.log('onDismiss');
@@ -68,7 +501,11 @@ webpackJsonp([2],{
 	                null,
 	                _react2.default.createElement(
 	                    _Popup2.default,
-	                    { className: 'fortest', transitionName: 'rmc-picker-popup-slide-fade', maskTransitionName: 'rmc-picker-popup-fade', content: 'popup', title: 'Picker', disabled: this.state.disabled, onDismiss: this.onDismiss, onOk: this.onOk },
+	                    { className: 'fortest', transitionName: 'rmc-picker-popup-slide-fade', maskTransitionName: 'rmc-picker-popup-fade', picker: _react2.default.createElement(
+	                            _MultiPicker2.default,
+	                            null,
+	                            [{ props: { children: colData } }, { props: { children: colData } }]
+	                        ), title: 'Picker', disabled: this.state.disabled, onDismiss: this.onDismiss, onOk: this.onOk, value: this.state.value },
 	                    _react2.default.createElement(
 	                        'button',
 	                        { disabled: this.state.disabled },
@@ -78,8 +515,7 @@ webpackJsonp([2],{
 	            )
 	        );
 	    }
-	}); /* tslint:disable:no-console */
-	
+	});
 	_reactDom2.default.render(_react2.default.createElement(Demo, null), document.getElementById('__react-content'));
 
 /***/ },
@@ -165,7 +601,7 @@ webpackJsonp([2],{
 	            )
 	          )
 	        ),
-	        this.props.content
+	        this.getContent()
 	      )
 	    );
 	  },
@@ -2419,6 +2855,10 @@ webpackJsonp([2],{
 	    value: true
 	});
 	
+	var _defineProperty2 = __webpack_require__(75);
+	
+	var _defineProperty3 = _interopRequireDefault(_defineProperty2);
+	
 	var _react = __webpack_require__(42);
 	
 	var _react2 = _interopRequireDefault(_react);
@@ -2431,6 +2871,8 @@ webpackJsonp([2],{
 	        return {
 	            onVisibleChange: noop,
 	            okText: 'Ok',
+	            pickerValueProp: 'selectedValue',
+	            pickerValueChangeProp: 'onValueChange',
 	            dismissText: 'Dismiss',
 	            title: '',
 	            onOk: noop,
@@ -2439,6 +2881,7 @@ webpackJsonp([2],{
 	    },
 	    getInitialState: function getInitialState() {
 	        return {
+	            pickerValue: null,
 	            visible: this.props.visible || false
 	        };
 	    },
@@ -2447,10 +2890,28 @@ webpackJsonp([2],{
 	            this.setVisibleState(nextProps.visible);
 	        }
 	    },
+	    onPickerChange: function onPickerChange(pickerValue) {
+	        if (this.state.pickerValue !== pickerValue) {
+	            this.setState({
+	                pickerValue: pickerValue
+	            });
+	            if (this.picker && this.picker.props[this.props.pickerValueChangeProp]) {
+	                this.picker.props[this.props.pickerValueChangeProp](pickerValue);
+	            }
+	        }
+	    },
+	    saveRef: function saveRef(picker) {
+	        this.picker = picker;
+	    },
 	    setVisibleState: function setVisibleState(visible) {
 	        this.setState({
 	            visible: visible
 	        });
+	        if (!visible) {
+	            this.setState({
+	                pickerValue: null
+	            });
+	        }
 	    },
 	    fireVisibleChange: function fireVisibleChange(visible) {
 	        if (this.state.visible !== visible) {
@@ -2491,8 +2952,17 @@ webpackJsonp([2],{
 	        this.fireVisibleChange(!this.state.visible);
 	    },
 	    onOk: function onOk() {
-	        this.props.onOk();
+	        this.props.onOk(this.picker && this.picker.getValue());
 	        this.fireVisibleChange(false);
+	    },
+	    getContent: function getContent() {
+	        if (this.props.picker) {
+	            var _React$cloneElement;
+	
+	            return _react2.default.cloneElement(this.props.picker, (_React$cloneElement = {}, (0, _defineProperty3.default)(_React$cloneElement, this.props.pickerValueProp, this.state.pickerValue || this.props.value), (0, _defineProperty3.default)(_React$cloneElement, this.props.pickerValueChangeProp, this.onPickerChange), (0, _defineProperty3.default)(_React$cloneElement, 'ref', this.saveRef), _React$cloneElement));
+	        } else {
+	            return this.props.content;
+	        }
 	    },
 	    onDismiss: function onDismiss() {
 	        this.props.onDismiss();
